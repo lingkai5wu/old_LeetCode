c45a549ebcccb556c5d61f033362dc1e832fadbf
lingkai5wu <1418605439@qq.com>
2022-02-14 21:40:13 +0800

    BST后续遍历

---
+++ src/maxSumBST/Solution.java
@@ -0,0 +1,43 @@
+package maxSumBST;
+
+import binaryTree.TreeNode;
+import binaryTree.TreeOperation;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	// TODO 自动生成的方法存根
+
+	TreeNode root = TreeOperation.stringTo("[1,4,3,2,4,2,5,null,null,null,null,null,null,4,6]");
+	System.out.println(new Solution().maxSumBST(root));
+    }
+
+    int maxSum;
+
+    public int maxSumBST(TreeNode root) {
+	traverse(root);
+	return maxSum;
+    }
+
+    int[] traverse(TreeNode root) {
+	if (root == null) {
+	    return new int[] { 1, Integer.MAX_VALUE, Integer.MIN_VALUE, 0 };
+	}
+
+	int[] left = traverse(root.left);
+	int[] right = traverse(root.right);
+
+	int[] res = new int[4]; // res的元素分别为：是否为BST、min、max、sum
+	if (left[0] == 1 && right[0] == 1 && root.val > left[2] && root.val < right[1]) {
+	    res[0] = 1;
+	    res[1] = Math.min(left[1], root.val);
+	    res[2] = Math.max(right[2], root.val);
+	    res[3] = left[3] + right[3] + root.val;
+	    maxSum = Math.max(maxSum, res[3]);
+	} else {
+	    res[0] = 0;
+	}
+
+	return res;
+    }
+}
