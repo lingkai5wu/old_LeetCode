a01a9b6654dcc188c11a72b1b250a0f3f22677d8
lingkai5wu <1418605439@qq.com>
2022-02-19 10:54:49 +0800

    每日一题

---
+++ src/pancakeSort/Solution.java
@@ -0,0 +1,53 @@
+package pancakeSort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	// TODO 自动生成的方法存根
+
+	int[] arr = { 3, 2, 4, 1 };
+	System.out.println(new Solution().pancakeSort(arr));
+    }
+
+    int[] arr;
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> pancakeSort(int[] arr) {
+	this.arr = arr;
+	sort(arr.length);
+	return res;
+    }
+
+    void sort(int n) {
+	if (n == 1) {
+	    return;
+	}
+
+	int max = arr[0], maxi = 0;
+	for (int i = 1; i < n; i++) {
+	    if (arr[i] > max) {
+		max = arr[i];
+		maxi = i;
+	    }
+	}
+
+	reverse(0, maxi);
+	res.add(maxi + 1);
+
+	reverse(0, n - 1);
+	res.add(n);
+
+	sort(n - 1);
+    }
+
+    void reverse(int i, int j) {
+	for (int temp; i < j; i++, j--) {
+	    temp = arr[i];
+	    arr[i] = arr[j];
+	    arr[j] = temp;
+	}
+    }
+}
