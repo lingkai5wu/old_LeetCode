d52a249fe2ce0e36f2103ed3e3bfc829e9528f67
lingkai5wu <1418605439@qq.com>
2022-02-28 20:44:51 +0800

    2Sum双指针

--- src/twoSum/Solution.java
+++ src/twoSum/Solution.java
@@ -1,5 +1,6 @@
 package twoSum;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,16 +14,33 @@ public class Solution {
 		int[] nums = { 2, 7, 11, 15 };
 		int target = 9;
 
-		IntegerArrayOperation.println(new Solution().twoSum(nums, target));
+		IntegerArrayOperation.println(new Solution().twoSum1(nums, target));
+		IntegerArrayOperation.println(new Solution().twoSum2(nums, target));
 	}
 
-	public int[] twoSum(int[] nums, int target) {
+	public int[] twoSum1(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<>();
 		for (int i = 0; i < nums.length; ++i) {
 			if (map.containsKey(target - nums[i])) {
 				return new int[] { map.get(target - nums[i]), i };
 			}
 			map.put(nums[i], i);
+		}
+		return new int[0];
+	}
+
+	public int[] twoSum2(int[] nums, int target) {
+		Arrays.sort(nums);
+		int lo = 0, hi = nums.length - 1;
+		while (lo < hi) {
+			int sum = nums[lo] + nums[hi];
+			if (sum < target) {
+				lo++;
+			} else if (sum > target) {
+				hi--;
+			} else {
+				return new int[] { lo, hi };
+			}
 		}
 		return new int[0];
 	}
