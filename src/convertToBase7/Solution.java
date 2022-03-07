ca9f01ccd5252fa91082cf95eaf7c541022d62bd
lingkai5wu <1418605439@qq.com>
2022-03-07 19:17:14 +0800

    每日一题

---
+++ src/convertToBase7/Solution.java
@@ -0,0 +1,27 @@
+package convertToBase7;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+		int num = -7;
+		System.out.println(new Solution().convertToBase7(num));
+	}
+
+	public String convertToBase7(int num) {
+		if (num == 0) {
+			return "0";
+		}
+		boolean flag = num < 0;
+		if (flag) {
+			num = -num;
+		}
+		StringBuilder sb = new StringBuilder();
+		while (num > 0) {
+			sb.append(num % 7);
+			num = num / 7;
+		}
+		return (flag ? "-" : "") + sb.reverse().toString();
+	}
+}
