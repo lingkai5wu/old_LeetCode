506b6c1f32644bb6b92f5671ca519862f316d8fc
lingkai5wu <1418605439@qq.com>
2022-03-24 21:16:47 +0800

    前缀和

---
+++ src/numMatrix/Solution.java
@@ -0,0 +1,33 @@
+package numMatrix;
+
+class NumMatrix {
+	int[][] preSum;
+
+	public NumMatrix(int[][] matrix) {
+		int m = matrix.length, n = matrix[0].length;
+		preSum = new int[m + 1][n + 1];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				preSum[i + 1][j + 1] = matrix[i][j] + preSum[i + 1][j] + preSum[i][j + 1] - preSum[i][j];
+			}
+		}
+	}
+
+	public int sumRegion(int row1, int col1, int row2, int col2) {
+		return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1];
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+		NumMatrix numMatrix = new NumMatrix(new int[][] { { 3, 0, 1, 4, 2 }, { 5, 6, 3, 2, 1 }, { 1, 2, 0, 1, 5 },
+				{ 4, 1, 0, 1, 7 }, { 1, 0, 3, 0, 5 } });
+		System.out.println(numMatrix.sumRegion(2, 1, 4, 3)); // return 8 (红色矩形框的元素总和)
+		System.out.println(numMatrix.sumRegion(1, 1, 2, 2)); // return 11 (绿色矩形框的元素总和)
+		System.out.println(numMatrix.sumRegion(1, 2, 2, 4)); // return 12 (蓝色矩形框的元素总和)
+	}
+
+}
