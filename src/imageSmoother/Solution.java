827ac0e3ba7a53471f48776329f2f1b570e42784
lingkai5wu <1418605439@qq.com>
2022-03-24 21:27:45 +0800

    增加前缀和解法~

--- src/imageSmoother/Solution.java
+++ src/imageSmoother/Solution.java
@@ -8,10 +8,11 @@ public class Solution {
 		// TODO 自动生成的方法存根
 
 		int[][] img = { { 100, 200, 100 }, { 200, 50, 200 }, { 100, 200, 100 } };
-		Int2dArrayOperation.println(new Solution().imageSmoother(img));
+		Int2dArrayOperation.println(new Solution().imageSmoother1(img));
+		Int2dArrayOperation.println(new Solution().imageSmoother2(img));
 	}
 
-	public int[][] imageSmoother(int[][] img) {
+	public int[][] imageSmoother1(int[][] img) {
 		int m = img.length, n = img[0].length;
 		int[][] res = new int[m][n];
 		int[][] dirs = new int[][] { { 0, 0 }, { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }, { -1, -1 }, { -1, 1 },
@@ -28,6 +29,27 @@ public class Solution {
 					sum += img[nx][ny];
 					cnt++;
 				}
+				res[i][j] = sum / cnt;
+			}
+		}
+		return res;
+	}
+
+	public int[][] imageSmoother2(int[][] img) {
+		int m = img.length, n = img[0].length;
+		int[][] res = new int[m][n], preSum = new int[m + 1][n + 1];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				preSum[i + 1][j + 1] = img[i][j] + preSum[i][j + 1] + preSum[i + 1][j] - preSum[i][j];
+			}
+		}
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				int x1 = Math.max(0, i - 1), x2 = Math.min(m - 1, i + 1);
+				int y1 = Math.max(0, j - 1), y2 = Math.min(n - 1, j + 1);
+				int sum = preSum[x2 + 1][y2 + 1] - preSum[x2 + 1][y1] - preSum[x1][y2 + 1] + preSum[x1][y1];
+				int cnt = (x2 - x1 + 1) * (y2 - y1 + 1);
 				res[i][j] = sum / cnt;
 			}
 		}
