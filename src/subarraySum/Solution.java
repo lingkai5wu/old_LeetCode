c9062dc8f29d7ee6d751faafd28da6b28e17aa0b
lingkai5wu <1418605439@qq.com>
2022-03-26 16:02:17 +0800

    前缀和

---
+++ src/subarraySum/Solution.java
@@ -0,0 +1,28 @@
+package subarraySum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+
+		int[] nums = { 1, -1, 0 };
+		int k = 0;
+		System.out.println(new Solution().subarraySum(nums, k));
+	}
+
+	public int subarraySum(int[] nums, int k) {
+		Map<Integer, Integer> preSum = new HashMap<>();
+		preSum.put(0, 1);
+		int res = 0, sum = 0;
+		int n = nums.length;
+		for (int i = 0; i < n; i++) {
+			sum += nums[i];
+			res += preSum.getOrDefault(sum - k, 0);
+			preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+		}
+		return res;
+	}
+}
